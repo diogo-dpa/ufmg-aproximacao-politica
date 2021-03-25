b43e8c9fdd7c0239c45c54403c8ea62e02f01ad9
Diogo Almazan <diogodpa@outlook.com>
2021-03-25 20:52:14 -0300

    Padronização dos botões e ajustes css globals

--- src/pages/cadastroConcluido.tsx
+++ src/pages/cadastroConcluido.tsx
@@ -1,11 +1,8 @@
 import Link from 'next/link'
-import Layout from '../components/Layout';
 
-import { ChevronLeft } from '@material-ui/icons'
 import styles from '../styles/pages/CadastroConcluido.module.css';
 
 const CadastroConcluido = () => (
-  // <Layout title="">
       <div className={styles.concludedRegisterContainer}>
 
         <img className={styles.imgConcludedRegister} src="/assets/greenCheck.svg" alt="Cadastro Finalizado"/>
@@ -14,11 +11,12 @@ const CadastroConcluido = () => (
             Você já pode acessar sua conta por meio do nosso login!
         </p>
 
-      <Link href="/">
-          <a className={styles.logIn}>Ir para login</a>
-      </Link>
+      <button type="button" className={styles.loginButton}>
+        <Link href="/">
+            <a >Ir para login</a>
+        </Link>
+      </button>
       </div>
-  // </Layout>
 )
 
 export default CadastroConcluido;
