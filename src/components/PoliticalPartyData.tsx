4acaf5937dfc71a1611e8fd2e2580fdf1a393bbc
Diogo Almazan <diogodpa@outlook.com>
2021-03-25 01:10:39 -0300

    Componente de Dados Numericos do Partido

---
+++ src/components/PoliticalPartyData.tsx
@@ -0,0 +1,21 @@
+import styles from '../styles/components/PoliticalPartyData.module.css';
+
+interface PoliticalPartyDataProps{
+    currentNumber: number;
+    totalNumber: number;
+    label: string;
+}
+
+const PoliticalPartyData = ({currentNumber, totalNumber, label}: PoliticalPartyDataProps) => {
+  return (
+    <div className={styles.partidoDataContainer}>
+        <div className={styles.numbersSection}>
+            <strong className={styles.currentNumber}>{currentNumber} </strong>
+            <span className={styles.totalNumber}>/{totalNumber}</span>
+        </div>
+        <span className={styles.statusData}>{label}</span>
+    </div>
+    );
+}
+
+export default PoliticalPartyData;
